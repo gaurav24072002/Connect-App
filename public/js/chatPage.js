77c20d114ae64141dd1ac9894a08dedad0329bbc
Gaurav Maheshwari <gaurav02maheshwari@gmail.com>
2021-06-15 17:28:28 +0530

    Updated the app with chat

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,23 +1,81 @@
 $(document).ready(() => {
-    $.get(`/api/chats/${chatId}`, (data) => {
-        $("#chatName").text(getChatName(data))
-    })
+    $.get(`/api/chats/${chatId}`, (data) => $("#chatName").text(getChatName(data)))
 })
 
 $("#chatNameButton").click(() => {
     var name = $("#chatNameTextbox").val().trim();
-
+    
     $.ajax({
         url: "/api/chats/" + chatId,
         type: "PUT",
         data: { chatName: name },
         success: (data, status, xhr) => {
-            if(xhr.status != 204){
-                alert("Could Not Update the Chat Name!!");
+            if(xhr.status != 204) {
+                alert("could not update");
             }
-            else{
+            else {
                 location.reload();
             }
         }
     })
 })
+
+$(".sendMessageButton").click(() => {
+    messageSubmitted();
+})
+
+$(".inputTextbox").keydown((event) => {
+
+    if(event.which === 13) {
+        messageSubmitted();
+        return false;
+    }
+})
+
+function messageSubmitted() {
+    var content = $(".inputTextbox").val().trim();
+
+    if(content != "") {
+        sendMessage(content);
+        $(".inputTextbox").val("");
+    }
+}
+
+function sendMessage(content) {
+    $.post("/api/messages", { content: content, chatId: chatId }, (data, status, xhr) => {
+        
+        if(xhr.status != 201){
+            alert("could not send Message");
+            $(".inputTextbox").val(content);
+            return;
+        }
+
+        addChatMessageHtml(data);
+
+    })
+}
+
+function addChatMessageHtml(message) {
+    if(!message || !message._id) {
+        alert("Message is not valid");
+        return;
+    }
+
+    var messageDiv = createMessageHtml(message);
+
+    $(".chatMessages").append(messageDiv);
+}
+
+function createMessageHtml(message) {
+
+    var isMine = message.sender._id == userLoggedIn._id;
+    var liClassName = isMine ? "mine" : "theirs";
+
+    return `<li class='message ${liClassName}'>
+                <div class='messageContainer'>
+                    <span class='messageBody'>
+                        ${message.content}
+                    </span>
+                </div>
+            </li>`;
+}
